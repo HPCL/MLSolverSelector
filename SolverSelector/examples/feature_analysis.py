d52de29f54abd31f5da335988f2eeb8f3dd10c26
Ben O'Neill <boneill@rnet-tech.com>
2018-11-29 11:03:03 -0500

    Clean up

--- SolverSelector/examples/feature_analysis.py
+++ SolverSelector/examples/feature_analysis.py
@@ -1,56 +1,84 @@
 
 
-### Format of the CSV is [ matrix name, matrix nnz, solve time, extraction time ] 
+### Format of the CSV is [ matrix name, matrix nnz, solve time, extraction time, extraction_time, extraction ] 
 
 import sys;
 import csv
+import matplotlib.pyplot as plt
 
-filename = sys.argv[1];
-header = None
-data = {}
-with open(filename) as csvfile:
+
+def getData(filename):
+  with open(filename) as csvfile:
+    
+    header = None
+    data = {}
+    data["nnz"] = []
+    data["solve"] = []
+    data["extract"] = []
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         if (header == None):
             header = row
         else :
-            data[row[0]] = [ int(row[1]), float(row[2]), float(row[3]) ] ; 
+            data["nnz"].append(int(row[1]))
+            data["solve"].append(float(row[2]))
+            for j in range(0,len(row[3:])):
+                if len(data["extract"]) < j+1 :
+                    data["extract"].append([])
+                data["extract"][j].append( float(row[3+j]) )
+    data["header"] = header
+    return data;
 
-import matplotlib.pyplot as plt
-x = []
-y = []
-z = []
-w = []
-for k in data.keys() :
-    x.append( data[k][0] )
-    y.append( data[k][1] )
-    z.append( data[k][2] )
-    w.append( data[k][2]/data[k][1] )
+COLORCOUNT=0;
+COLORS=['b','g','r','k','m']
 
-f = plt.figure();
-plt.scatter(x,y, alpha =0.5);
-ax = plt.gca()
-ax.set_xscale('log')
+def plotExtractionTimes(data, ax) :
+    COLORCOUNT = 0
+    for i in range(0, len(data["extract"])):
+        ax.scatter(data["nnz"], data["extract"][i] , c=COLORS[COLORCOUNT], alpha=0.5, label=data["header"][3+i])
+        COLORCOUNT+=1
+    ax.set_xscale('log')
+    ax.set_xlabel("Number of non zeros");
+    ax.set_ylabel("Extraction Time")
 
-plt.xlabel("Number of NonZeros");
-plt.ylabel("Solve Time (GMRES + ILU)");
+def plotSolveTimes(data, ax) :
+    COLORCOUNT=0
+    ax.scatter(data["nnz"], data["solve"] , alpha=0.5, label="Solve Time")
+    ax.set_xlabel("Number of non zeros");
+    ax.set_ylabel("Solve Time")
+    ax.legend()
+
+def plotExtractionRatio(data,ax):
+    COLORCOUNT = 0
+    for i in range(0, len(data["extract"])):
+        ratio = []
+        for j in range(0,len(data["extract"][i])):
+            ratio.append( data["extract"][i][j] / data["solve"][j] ) 
+        ax.scatter(data["nnz"], ratio , c=COLORS[COLORCOUNT], alpha=0.5, label=data["header"][3+i])
+        COLORCOUNT += 1
+    ax.legend()
+    ax.set_xscale('log')
+    ax.set_xlabel("Number of non zeros");
+    ax.set_ylabel("Extraction Time/Solve Time")
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+
 
 plt.figure()
-plt.scatter(x,z, alpha=0.5);
-ax = plt.gca()
-ax.set_xscale('log')
-plt.xlabel("Number of NonZeros");
-plt.ylabel("Extraction Time");
-
+extract_ax = plt.gca()
 plt.figure()
-plt.scatter(x,w, alpha=0.5);
-ax = plt.gca()
-ax.set_xscale('log')
-plt.xlabel("Number of NonZeros");
-plt.ylabel("Extraction Time / Solve Time ");
+solve_ax = plt.gca()
+plt.figure()
+ratio_ax = plt.gca()
 
-plt.show();
+for ii in range(1, len(sys.argv)):
+    data = getData(sys.argv[ii])
+    plotExtractionTimes(data, extract_ax)
+    plotSolveTimes(data,solve_ax)
+    plotExtractionRatio(data,ratio_ax)
 
+plt.show()
 
 
 
